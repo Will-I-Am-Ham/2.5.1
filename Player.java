97a0d7ddfadd5d930a0b8c5c25676b91a541c25e
zengjosh <zeng080407@gmail.com>
2023-10-26 11:40:26 -0700

    .\Player.java

--- Player.java
+++ Player.java
@@ -9,10 +9,30 @@ import java.util.Scanner;
 public class Player
 {
   /* your code here - attributes */
-  
+  private String name = ""; 
+  private double points;
   /* your code here - constructor(s) */ 
+  public Player() {
+    System.out.println("Enter Player Name: ");
+    Scanner sc = new Scanner(System.in);
+    String newName = sc.nextLine();
 
+    name = newName;
+    System.out.println("Hello and welcome to the game, " + name);
+  }
   /* your code here - accessor(s) */ 
-
+  public String getName()
+  {
+    return name;
+  }
+  
+  public double getPoints()
+  {
+    return points;
+  }
   /* your code here - mutator(s) */ 
+  public void setName(String x)
+  {
+    name = x;
+  }
 }
